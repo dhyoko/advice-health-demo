cd0e7d372071614edce8f5ad27a25687eca003ee
Diogo Yokoyama <dih.yokoyama@gmail.com>
2023-09-06 06:30:44 -0300

    Refactor layout component

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { get } from "lodash";
 import { ContainerWrapper } from "./style";
 import Header from "../header";
 
-const Layout = ({ children }) => (
-  <>
-    <Header />
-    <ContainerWrapper>
-      <Container fluid>{children}</Container>
-    </ContainerWrapper>
-  </>
-);
+const Layout = (props) => {
+  const title = get(props, "title", "");
+  const children = get(props, "children", null);
 
+  return (
+    <>
+      <Header />
+      <ContainerWrapper>
+        <Container fluid>
+          <h1 className="page-title">{title}</h1>
+          {children}
+        </Container>
+      </ContainerWrapper>
+    </>
+  );
+};
 export default Layout;
--- src/components/layout/style.js
+++ src/components/layout/style.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
-import { HEADER_HEIGHT } from "../../utils/consts";
+import { COLORS, HEADER_HEIGHT } from "../../utils/consts";
 
 export const ContainerWrapper = styled.div`
   margin-top: ${HEADER_HEIGHT};
   padding: 0 15px;
+
+  .page-title {
+    color: ${COLORS.SOFT_BLACK};
+  }
 `;
